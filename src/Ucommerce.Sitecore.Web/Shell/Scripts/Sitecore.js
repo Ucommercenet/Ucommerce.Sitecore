2f7e752f0ebdb32e8e7e9497415d25b72ea3789a
MSK <devnull@localhost>
2018-05-24 21:36:02 +0200

    Added constants file for shell.

---
+++ src/Ucommerce.Sitecore.Web/Shell/Scripts/Sitecore.js
@@ -0,0 +1,10 @@
+﻿var constants = {
+    baseurl: '',
+	webPageBaseUrl: '/sitecore modules/Shell/ucommerce/',
+	serviceBaseUrl: '/ucommerceapi/'
+};
+
+var UCommerceClientMgr = {
+	BaseUCommerceUrl: constants.webPageBaseUrl,
+	Shell: 'Sitecore'
+}
