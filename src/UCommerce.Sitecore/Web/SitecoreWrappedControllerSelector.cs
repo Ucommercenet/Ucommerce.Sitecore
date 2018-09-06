c31e9bdbb27141de181b92d8d15605f5ac7d4d78
MSK <devnull@localhost>
2018-09-06 22:34:26 +0200

    Reflection to support version 82 and 90 of Sitecore.

--- src/UCommerce.Sitecore/Web/SitecoreWrappedControllerSelector.cs
+++ src/UCommerce.Sitecore/Web/SitecoreWrappedControllerSelector.cs
@@ -4,7 +4,10 @@ using System.Web.Http.Controllers;
 using System.Web.Http.Dispatcher;
 using Sitecore.Services.Core;
 using System.Net.Http;
+using System.Reflection;
 using Sitecore.Services.Infrastructure.Web.Http.Dispatcher;
+using UCommerce.Sitecore.Logging;
+using UCommerce.Sitecore.UI.Resources;
 
 namespace UCommerce.Sitecore.Web
 {
@@ -14,8 +17,38 @@ namespace UCommerce.Sitecore.Web
 
         public SitecoreWrappedControllerSelector(HttpConfiguration configuration, IControllerNameGenerator controllerNameGenerator) : base(configuration)
         {
-            _sitecoreHttpControllerSelector = new NamespaceHttpControllerSelector(configuration, controllerNameGenerator);
-            
+            SitecoreVersionResolver vr = new SitecoreVersionResolver(new LoggingService());
+            if (vr.IsEqualOrGreaterThan(new Version(9, 0)))
+            {
+                _sitecoreHttpControllerSelector = GetControllerSelectorFromReflectionVersion90(configuration, controllerNameGenerator);
+            }
+            else
+            {
+                _sitecoreHttpControllerSelector = GetControllerSelectorFromReflectionVersion82(configuration, controllerNameGenerator);
+            }
+        }
+
+        private IHttpControllerSelector GetControllerSelectorFromReflectionVersion90(HttpConfiguration configuration, IControllerNameGenerator controllerNameGenerator)
+        {
+            ConstructorInfo constructor = typeof(NamespaceHttpControllerSelector).GetConstructor(new[]
+                {
+                    typeof(HttpConfiguration),
+                    typeof(IControllerNameGenerator),
+                    typeof(IHttpControllerSelector)
+                });
+
+            return (IHttpControllerSelector)constructor.Invoke(new object[] { configuration, controllerNameGenerator, this });
+        }
+
+        private IHttpControllerSelector GetControllerSelectorFromReflectionVersion82(HttpConfiguration configuration, IControllerNameGenerator controllerNameGenerator)
+        {
+            ConstructorInfo constructor = typeof(NamespaceHttpControllerSelector).GetConstructor(new[]
+            {
+                typeof(HttpConfiguration),
+                typeof(IControllerNameGenerator),
+            });
+
+            return (IHttpControllerSelector) constructor.Invoke(new object[] {configuration, controllerNameGenerator});
         }
 
         public override HttpControllerDescriptor SelectController(HttpRequestMessage request)
