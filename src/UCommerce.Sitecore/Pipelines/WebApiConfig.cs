a12de0a96340dc6a6540c646e8a3a390ca9583e5
Petr Ilnytsky <devnull@localhost>
2018-12-14 11:33:24 +0100

    Removed unused references

--- src/UCommerce.Sitecore/Pipelines/WebApiConfig.cs
+++ src/UCommerce.Sitecore/Pipelines/WebApiConfig.cs
@@ -1,7 +1,6 @@
 ﻿using System.Web.Http;
 using System.Web.Http.Dispatcher;
 using Sitecore.Pipelines;
-using Sitecore.Services.Core;
 using UCommerce.Sitecore.Web;
 
 namespace UCommerce.Sitecore.Pipelines
