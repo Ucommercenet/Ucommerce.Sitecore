efd4b521ad5a3be20947e0963e2dfed2764dee57
Søren Skovsbøll <soren@skovsboll.com>
2019-12-12 13:00:37 +0100

    update version and fix broken references

--- src/UCommerce.Sitecore.CommerceConnect/Properties/AssemblyInfo.cs
+++ src/UCommerce.Sitecore.CommerceConnect/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("8.3.0.19276")]
-[assembly: AssemblyVersion("8.3.0.19276")]
-[assembly: AssemblyFileVersion("8.3.0.19276")]
-[assembly: AssemblyInformationalVersion("8.3.0.19276 ")]
+// [assembly: AssemblyVersion("8.4.0.19345")]
+[assembly: AssemblyVersion("8.4.0.19345")]
+[assembly: AssemblyFileVersion("8.4.0.19345")]
+[assembly: AssemblyInformationalVersion("8.4.0.19345 7cdca61c87846811aff1688ad90bb21c7b9afcec")]
--- src/UCommerce.Sitecore92/Properties/AssemblyInfo.cs
+++ src/UCommerce.Sitecore92/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("8.3.0.19276")]
-[assembly: AssemblyVersion("8.3.0.19276")]
-[assembly: AssemblyFileVersion("8.3.0.19276")]
+// [assembly: AssemblyVersion("8.4.0.19345")]
+[assembly: AssemblyVersion("8.4.0.19345")]
+[assembly: AssemblyFileVersion("8.4.0.19345")]
