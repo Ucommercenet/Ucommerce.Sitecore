c060d9daa6859b316fc67b6eeb0add6414a7fe02
Søren Skovsbøll <soren@skovsboll.com>
2019-12-20 11:54:57 +0100

    update assemblyinfo and package refs for 8.4.1 release

--- src/UCommerce.Sitecore/Properties/AssemblyInfo.cs
+++ src/UCommerce.Sitecore/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("8.4.0.19345")]
-[assembly: AssemblyVersion("8.4.0.19345")]
-[assembly: AssemblyFileVersion("8.4.0.19345")]
-[assembly: AssemblyInformationalVersion("8.4.0.19345 7cdca61c87846811aff1688ad90bb21c7b9afcec")]
+// [assembly: AssemblyVersion("8.4.1.19354")]
+[assembly: AssemblyVersion("8.4.1.19354")]
+[assembly: AssemblyFileVersion("8.4.1.19354")]
+[assembly: AssemblyInformationalVersion("8.4.1.19354 efd4b521ad5a3be20947e0963e2dfed2764dee57")]
--- src/UCommerce.Sitecore92/Properties/AssemblyInfo.cs
+++ src/UCommerce.Sitecore92/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("8.4.0.19345")]
-[assembly: AssemblyVersion("8.4.0.19345")]
-[assembly: AssemblyFileVersion("8.4.0.19345")]
+// [assembly: AssemblyVersion("8.4.1.19354")]
+[assembly: AssemblyVersion("8.4.1.19354")]
+[assembly: AssemblyFileVersion("8.4.1.19354")]
