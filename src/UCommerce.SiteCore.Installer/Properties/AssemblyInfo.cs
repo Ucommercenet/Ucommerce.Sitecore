9a40448f00363cb497408dfe65ee2422820f5752
Søren Skovsbøll <soren@skovsboll.com>
2020-06-23 12:53:57 +0200

    fix: upgrade package references to latest Ucommerce verson

--- src/UCommerce.SiteCore.Installer/Properties/AssemblyInfo.cs
+++ src/UCommerce.SiteCore.Installer/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("9.1.0.20154")]
-[assembly: AssemblyVersion("9.1.0.20154")]
-[assembly: AssemblyFileVersion("9.1.0.20154")]
-[assembly: AssemblyInformationalVersion("9.1.0.20154 52f413f6cfc5658bb9f6c4a0c97a878b079c1f90")]
+// [assembly: AssemblyVersion("9.1.0.20163")]
+[assembly: AssemblyVersion("9.1.0.20163")]
+[assembly: AssemblyFileVersion("9.1.0.20163")]
+[assembly: AssemblyInformationalVersion("9.1.0.20163 e2196dddc8169185107ad4ffec4d216e7c720abc")]
--- src/Ucommerce.Sitecore93/Properties/AssemblyInfo.cs
+++ src/Ucommerce.Sitecore93/Properties/AssemblyInfo.cs
@@ -1,35 +1,34 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ucommerce.Sitecore92")]
+[assembly: AssemblyTitle("Ucommerce.Sitecore93.Compatibility")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ucommerce.Sitecore92")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("Ucommerce.Sitecore93.Compatibility")]
+[assembly: AssemblyCopyright("Copyright �  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c46c6742-a928-4a0d-bf86-485bcae76648")]
+[assembly: Guid("BEC8A6FB-3563-4DB7-A915-90D8BC72F99B")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("9.1.0.20163")]
 [assembly: AssemblyVersion("9.1.0.20163")]
