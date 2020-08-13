680534efb0cfc9d7ba7ddb887c8b5d0e98dbaedb
David <david.nkelemen@gmail.com>
2020-08-13 10:33:34 +0200

    fix: Exception when requesting a media item url

---
+++ src/Ucommerce.Sitecore93/Content/SitecoreImageService.cs
@@ -0,0 +1,20 @@
+using Sitecore.Data.Items;
+using Sitecore.Links.UrlBuilders;
+using Sitecore.Resources.Media;
+using Ucommerce.Infrastructure.Logging;
+using Ucommerce.Sitecore;
+
+namespace Ucommerce.Sitecore93.Content
+{
+    public class SitecoreImageService : Ucommerce.Sitecore.Content.SitecoreImageService
+    	{
+	        protected override string GetMediaUrl(Item item)
+            {
+	            return MediaManager.GetMediaUrl(item, new MediaUrlBuilderOptions { AlwaysIncludeServerUrl = true });
+            }
+
+	        public SitecoreImageService(ILoggingService loggingService, ISitecoreContext sitecoreContext) : base(loggingService, sitecoreContext)
+	        {
+	        }
+        }
+}
