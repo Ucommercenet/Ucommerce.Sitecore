cceef58202071344626636353128f0ce91067692
Skidaddle99 <Mads.Bjernemose.Nielsen@ucommerce.dk>
2023-01-17 09:11:02 +0100

    Added "Steps" folder

--- src/Ucommerce.Sitecore.Cli/Program.cs
+++ src/Ucommerce.Sitecore.Cli/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace Ucommerce.Sitecore.Cli
+﻿using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+
+namespace Ucommerce.Sitecore.Cli
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        public static async Task<int> Main()
         {
+            return await new CliApplicationBuilder()
+                .AddCommandsFromThisAssembly()
+                .Build()
+                .RunAsync();
+        }
+    }
+
+    [Command("Hello")]
+    public class HelloWorld : ICommand
+    {
+        public ValueTask ExecuteAsync(IConsole console)
+        {
+            console.Output.WriteLine("Hello world");
+
+            return default;
         }
     }
 }
