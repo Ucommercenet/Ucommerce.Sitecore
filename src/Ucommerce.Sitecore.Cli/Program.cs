b20820a40caed49ddbcbe05b90d42f4c9f4e040d
Nads Mielsen <106595231+Skidaddle99@users.noreply.github.com>
2023-01-17 15:21:26 +0100

    Ucommerce Sitecore CLI project with CliFx - Initial commit (#77)
    * Ucommerce Sitecore CLI with NConsole project - Initial commit
    
    * Added "Steps" folder

---
+++ src/Ucommerce.Sitecore.Cli/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+
+namespace Ucommerce.Sitecore.Cli
+{
+    internal class Program
+    {
+        public static async Task<int> Main()
+        {
+            return await new CliApplicationBuilder()
+                .AddCommandsFromThisAssembly()
+                .Build()
+                .RunAsync();
+        }
+    }
+
+    [Command("Hello")]
+    public class HelloWorld : ICommand
+    {
+        public ValueTask ExecuteAsync(IConsole console)
+        {
+            console.Output.WriteLine("Hello world");
+
+            return default;
+        }
+    }
+}
