4068867b46ca0b53657fa0d00138957f56c0bf93
Nads Mielsen <106595231+Skidaddle99@users.noreply.github.com>
2023-01-18 09:34:32 +0100

    Initial command scaffolding (#78)
    * Initial command scaffolding

---
+++ src/Ucommerce.Sitecore.Cli/Commands/Install.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+
+namespace Ucommerce.Sitecore.Cli.Commands
+{
+    [Command("install")]
+    public class Install : ICommand
+    {
+        [CommandParameter(0, Description = "Connection string for the database.")]
+        public string ConnectionString { get; set; }
+
+        [CommandParameter(1, Description = "Path to Sitecore.")]
+        public string SitecorePath { get; set; }
+
+        [CommandOption("backupdb", 'b', Description = "Do a backup of the current database.")]
+        public bool BackupDatabase { get; set; } = false;
+
+        [CommandOption("upgradedb", 'u', Description = "Upgrade the database.")]
+        public bool UpgradeDatabase { get; set; } = false;
+
+        public virtual ValueTask ExecuteAsync(IConsole console)
+        {
+            console.Output.WriteLine("Installing..");
+            if (BackupDatabase) console.Output.WriteLine("Backing up database..");
+            if (UpgradeDatabase) console.Output.WriteLine("Upgrading database..");
+
+            return default;
+        }
+    }
+}
---
+++ src/Ucommerce.Sitecore.Cli/Commands/Upgrade.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+
+namespace Ucommerce.Sitecore.Cli.Commands
+{
+    [Command("upgrade")]
+    public class Upgrade : Install
+    {
+        public override ValueTask ExecuteAsync(IConsole console)
+        {
+            console.Output.WriteLine("Upgrading..");
+            if (BackupDatabase) console.Output.WriteLine("Backing up database..");
+            if (UpgradeDatabase) console.Output.WriteLine("Upgrading database..");
+
+            return default;
+        }
+    }
+}
