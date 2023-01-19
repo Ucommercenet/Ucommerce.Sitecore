13e7251c65dfb81162800044a55bc804610d9c87
Nads Mielsen <106595231+Skidaddle99@users.noreply.github.com>
2023-01-19 13:39:39 +0100

    chore: create move directory step (#93)

--- src/UCommerce.SiteCore.Installer/Steps/MoveDirectory.cs
+++ src/UCommerce.SiteCore.Installer/Steps/MoveDirectory.cs
@@ -1,34 +1,39 @@
-using System.Collections.Specialized;
+using System;
 using System.IO;
-using System.Web.Hosting;
-using Sitecore.Install.Framework;
+using System.Threading.Tasks;
 using Ucommerce.Installer;
 
 namespace Ucommerce.Sitecore.Installer.Steps
 {
-	/// <summary>
-	/// Moves a folder.
-	/// </summary>
-	public class MoveDirectory : IPostStep
-	{
-		private readonly string _sourceDirectory;
-		private readonly string _targetDirectory;
-		private readonly bool _overwriteTarget;
+    /// <summary>
+    ///     Moves a folder.
+    /// </summary>
+    public class MoveDirectory : IStep
+    {
+        private readonly IInstallerLoggingService _loggingService;
+        private readonly bool _overwriteTarget;
+        private readonly DirectoryInfo _sourceDirectory;
+        private readonly DirectoryInfo _targetDirectory;
 
-		public MoveDirectory(string sourceDirectory, string targetDirectory, bool overwriteTarget)
-		{
-			_sourceDirectory = sourceDirectory;
-			_targetDirectory = targetDirectory;
-			_overwriteTarget = overwriteTarget;
-		}
+        public MoveDirectory(DirectoryInfo sourceDirectory,
+            DirectoryInfo targetDirectory,
+            bool overwriteTarget,
+            IInstallerLoggingService loggingService)
+        {
+            _sourceDirectory = sourceDirectory;
+            _targetDirectory = targetDirectory;
+            _overwriteTarget = overwriteTarget;
+            _loggingService = loggingService;
+        }
 
-		public void Run(ITaskOutput output, NameValueCollection metaData)
-		{
-			new DirectoryMover(
-				new DirectoryInfo(HostingEnvironment.MapPath(_sourceDirectory)),
-				new DirectoryInfo(HostingEnvironment.MapPath(_targetDirectory)),
-				_overwriteTarget).Move(
-				ex => new SitecoreInstallerLoggingService().Error<int>(ex));
-		}
-	}
+        public async Task Run()
+        {
+            Console.WriteLine("Moving " + _sourceDirectory + " to " + _targetDirectory);
+            new DirectoryMover(
+                _sourceDirectory,
+                _targetDirectory,
+                _overwriteTarget).Move(
+                ex => _loggingService.Error<int>(ex));
+        }
+    }
 }
