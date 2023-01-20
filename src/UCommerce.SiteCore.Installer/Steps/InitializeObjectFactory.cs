9774f2691acd4d07ed9930708ed758989c7be640
Maher Hussain <maherhussain6@gmail.com>
2023-01-20 14:24:12 +0100

    chore: initialize object factory step

--- src/UCommerce.SiteCore.Installer/Steps/InitializeObjectFactory.cs
+++ src/UCommerce.SiteCore.Installer/Steps/InitializeObjectFactory.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections.Specialized;
-using Sitecore.Install.Framework;
+using System.Threading.Tasks;
 using Ucommerce.Installer;
 
 namespace Ucommerce.Sitecore.Installer.Steps
 {
-	public class InitializeObjectFactory : IPostStep
-	{
-		public void Run(ITaskOutput output, NameValueCollection metaData)
+   
+    public class InitializeObjectFactory : IStep
+	{ 
+		private readonly IInstallerLoggingService _loggingService;
+
+		public InitializeObjectFactory (IInstallerLoggingService loggingService)
 		{
+			_loggingService = loggingService;
+		}
+        public async Task Run()
+		{
+			_loggingService.Information<InitializeObjectFactory>("initialize object factory"); 
 			var initializer = new ObjectFactoryInitializer();
 			initializer.InitializeObjectFactory();
 		}
