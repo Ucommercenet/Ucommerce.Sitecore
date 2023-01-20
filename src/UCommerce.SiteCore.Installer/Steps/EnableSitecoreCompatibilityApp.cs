ddd263928e252fb868f58ace102d62ba9c1c97dd
Maher Hussain <maherhussain6@gmail.com>
2023-01-20 15:06:28 +0100

    chore: enable sitecore compatibility app

--- src/UCommerce.SiteCore.Installer/Steps/EnableSitecoreCompatibilityApp.cs
+++ src/UCommerce.SiteCore.Installer/Steps/EnableSitecoreCompatibilityApp.cs
@@ -1,34 +1,37 @@
 using System;
 using System.Collections.Specialized;
 using System.IO;
+using System.Threading.Tasks;
 using System.Web.Hosting;
-using Sitecore.Install.Framework;
 using Ucommerce.Installer;
 
 namespace Ucommerce.Sitecore.Installer.Steps
 {
-    public class EnableSitecoreCompatibilityApp : IPostStep
+    public class EnableSitecoreCompatibilityApp : IStep
     {
         private readonly SitecoreVersionChecker _sitecoreVersionChecker;
         private readonly IInstallerLoggingService _sitecoreInstallerLoggingService;
+        private readonly DirectoryInfo _baseDirectory;
 
-        public EnableSitecoreCompatibilityApp(SitecoreVersionChecker sitecoreVersionChecker,
+        public EnableSitecoreCompatibilityApp(SitecoreVersionChecker sitecoreVersionChecker, DirectoryInfo baseDirectory,
             IInstallerLoggingService sitecoreInstallerLoggingService)
         {
             _sitecoreVersionChecker = sitecoreVersionChecker;
             _sitecoreInstallerLoggingService = sitecoreInstallerLoggingService;
+            _baseDirectory = baseDirectory;
         }
 
-        public void Run(ITaskOutput output, NameValueCollection metaData)
+        public async Task Run()
         {
-            string _virtualPathToAppsFolder = "~/sitecore modules/shell/ucommerce/apps";
+            _sitecoreInstallerLoggingService.Information<EnableSitecoreCompatibilityApp>("Enable sitecore compatibility app");
+            var _virtualPathToAppsFolder = new DirectoryInfo(Path.Combine(_baseDirectory.FullName, "sitecore modules", "shell", "ucommerce", "apps"));
             if (_sitecoreVersionChecker.IsEqualOrGreaterThan(new Version(9, 2)))
             {
                 new DirectoryMover(
                     new DirectoryInfo(
-                        HostingEnvironment.MapPath($"{_virtualPathToAppsFolder}/Sitecore92compatibility.disabled")),
+                        Path.Combine(_virtualPathToAppsFolder.FullName, "Sitecore92compatibility.disabled")),
                     new DirectoryInfo(
-                        HostingEnvironment.MapPath($"{_virtualPathToAppsFolder}/Sitecore92compatibility")),
+                       Path.Combine(_virtualPathToAppsFolder.FullName, "Sitecore92compatibility")),
                     true).Move(ex => _sitecoreInstallerLoggingService.Error<Exception>(ex));
             }
 
@@ -38,9 +41,9 @@ namespace Ucommerce.Sitecore.Installer.Steps
 
                 new DirectoryMover(
                     new DirectoryInfo(
-                        HostingEnvironment.MapPath($"{_virtualPathToAppsFolder}/Sitecore93compatibility.disabled")),
+                        Path.Combine(_virtualPathToAppsFolder.FullName, "Sitecore93compatibility.disabled")),
                     new DirectoryInfo(
-                        HostingEnvironment.MapPath($"{_virtualPathToAppsFolder}/Sitecore93compatibility")),
+                      Path.Combine(_virtualPathToAppsFolder.FullName, "Sitecore93compatibility")),
                     true).Move(ex => _sitecoreInstallerLoggingService.Error<Exception>(ex));
             }
         }
