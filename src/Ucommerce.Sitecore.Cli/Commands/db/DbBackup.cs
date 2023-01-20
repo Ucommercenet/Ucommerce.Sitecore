2114b9f4095735286968dbafac65b81198f5f78b
Rasmus Toft Lauridsen <rasmus.lauridsen@ucommerce.net>
2023-01-20 12:57:06 +0100

    refactor: split db commands into separate files
    Additionally we have changed the signature of the InstallDatabase step
    to take the logger as the last parameter.
    
    sc-18443

---
+++ src/Ucommerce.Sitecore.Cli/Commands/db/DbBackup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+using Ucommerce.Sitecore.Cli.Logging;
+using Ucommerce.Sitecore.Installer.Steps;
+
+namespace Ucommerce.Sitecore.Cli.Commands.db
+{
+    [Command("db backup")]
+    // ReSharper disable once UnusedType.Global
+    public class DbBackup : Db, ICommand
+    {
+        public async ValueTask ExecuteAsync(IConsole console)
+        {
+            var logging = new CliLogger(console);
+            logging.Information<DbBackup>("Backing up database...");
+
+            var baseDirectory = new DirectoryInfo(AppContext.BaseDirectory);
+            var databaseStep = new DatabaseStep(ConnectionString, baseDirectory, logging, backupDb: true);
+            await databaseStep.Run();
+        }
+    }
+}
---
+++ src/Ucommerce.Sitecore.Cli/Commands/db/DbInstall.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+using Ucommerce.Sitecore.Cli.Logging;
+using Ucommerce.Sitecore.Installer.Steps;
+
+namespace Ucommerce.Sitecore.Cli.Commands.db
+{
+    [Command("db install", Description = "Install a fresh Ucommerce Database")]
+    // ReSharper disable once UnusedType.Global
+    public class DbInstall : Db, ICommand
+    {
+        public async ValueTask ExecuteAsync(IConsole console)
+        {
+            var logging = new CliLogger(console);
+            logging.Information<DbInstall>("Installing database...");
+
+            var baseDirectory = new DirectoryInfo(AppContext.BaseDirectory);
+            var databaseStep = new DatabaseStep(ConnectionString, baseDirectory, logging, upgradeDb: true);
+            await databaseStep.Run();
+        }
+    }
+}
