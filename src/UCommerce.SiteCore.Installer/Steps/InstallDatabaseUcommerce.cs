74fe52d5cbdc33b2e5338ec0364ec1679c6b8541
Rasmus Toft Lauridsen <rasmus.lauridsen@ucommerce.net>
2023-01-20 15:16:20 +0100

    refactor: split db steps into separate classes
    Makes easier to reason about the functionality of the individual steps.
    Single responsibility for the win
    
    Moved the compositiion root to the command level.
    This makes it easier to change dependencies based on use case.
    Also prevents instantiating the same class multiple times.
    
    sc-18471

---
+++ src/UCommerce.SiteCore.Installer/Steps/InstallDatabaseUcommerce.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Ucommerce.Installer;
+
+namespace Ucommerce.Sitecore.Installer.Steps
+{
+    public class InstallDatabaseUcommerce : IStep
+    {
+        private readonly DbInstaller _command;
+
+        public InstallDatabaseUcommerce(DirectoryInfo packageBasePath, InstallationConnectionStringLocator connectionStringLocator, IInstallerLoggingService logging)
+        {
+            var migrationsDirectory =
+                new DirectoryInfo(Path.Combine(packageBasePath.FullName, "package", "files", "sitecore modules", "Shell", "Ucommerce", "Install"));
+            var migrations = new MigrationLoader().GetDatabaseMigrations(migrationsDirectory);
+
+            _command = new DbInstallerCore(connectionStringLocator, migrations, logging);
+        }
+
+        public async Task Run()
+        {
+            _command.InstallDatabase();
+        }
+    }
+}
