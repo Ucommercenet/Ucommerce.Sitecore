17f58bc1a7683ea5ecc8378fc89e2abeb58380b8
Skidaddle99 <Mads.Bjernemose.Nielsen@ucommerce.dk>
2023-01-24 15:42:19 +0100

    chore: create proper console logger
    Added two Log methods for formatting the text better, and added a few more details to information and error logging like color and log level
    
    sc-18456

--- src/Ucommerce.Sitecore.Cli/Logging/CliLogger.cs
+++ src/Ucommerce.Sitecore.Cli/Logging/CliLogger.cs
@@ -14,30 +14,59 @@ namespace Ucommerce.Sitecore.Cli.Logging
             _console = console;
         }
 
+        public void Error<T>(Exception exception)
+        {
+            Log("Error", ConsoleColor.Red, "Exception occured.", typeof(T), exception);
+        }
+
+        public void Error<T>(Exception exception, string message)
+        {
+            Log("Error", ConsoleColor.Red, message, typeof(T), exception);
+        }
+
+        public void Error<T>(Exception exception, string messageTemplate, params object[] propertyValues)
+        {
+            Log("Error", ConsoleColor.Red, messageTemplate, typeof(T), exception);
+        }
+
         public void Information<T>(string message)
         {
-            _console.Output.WriteLine(message);
+            Log("Information", ConsoleColor.Green, message, typeof(T));
         }
 
         public void Information<T>(string messageTemplate, params object[] propertyValues)
         {
             MessageTemplateParser.TryParse(messageTemplate, propertyValues, out var message);
-            _console.Output.WriteLine(message);
+            Log("Information", ConsoleColor.Green, message, typeof(T));
         }
 
-        public void Error<T>(Exception exception)
+        private void Log(string logLevel,
+            ConsoleColor color,
+            string message,
+            Type type,
+            Exception exception = null)
         {
-            _console.Output.WriteLine(exception.Message);
-        }
+            var originalColor = Console.ForegroundColor;
 
-        public void Error<T>(Exception exception, string message)
-        {
-            _console.Output.WriteLine(exception.Message);
-        }
+            _console.ForegroundColor = color;
+            _console.Output.WriteLine($"[{logLevel}]");
 
-        public void Error<T>(Exception exception, string messageTemplate, params object[] propertyValues)
-        {
-            _console.Output.WriteLine(exception.Message);
+            _console.ForegroundColor = originalColor;
+            _console.Output.Write($"     {type.Name} - ");
+
+            _console.ForegroundColor = color;
+
+            if (exception == null)
+            {
+                _console.Output.WriteLine($"{message}");
+            }
+            else
+            {
+                _console.Error.WriteLine($"{message}");
+                _console.Error.WriteLine($"     {exception.Message}{(exception.StackTrace != null ? $"\n\n{exception.StackTrace}" : "")}");
+            }
+
+            _console.ForegroundColor = originalColor;
         }
     }
 }
