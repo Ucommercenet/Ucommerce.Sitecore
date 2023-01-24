3d362b2dcb54fd869648f50f15ac4891a35327a2
Nads Mielsen <106595231+Skidaddle99@users.noreply.github.com>
2023-01-24 11:41:29 +0100

    Chore: Create MergeConfig step (#114)
    * Chore: Create MergeConfig step

---
+++ src/UCommerce.SiteCore.Installer/Steps/Transformation.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace Ucommerce.Sitecore.Installer.Steps
+{
+    public class Transformation
+    {
+        public Transformation(FileInfo path, bool isIntegrated = false)
+        {
+            Path = path;
+            OnlyIfIisIntegrated = isIntegrated;
+        }
+
+        public bool OnlyIfIisIntegrated { get; set; }
+        public FileInfo Path { get; set; }
+    }
+}
