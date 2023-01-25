9b75390149afa2360356c4d684cb9649c65c27f5
Maher Hussain <maherhussain6@gmail.com>
2023-01-25 10:47:44 +0100

    chore:(upgrade.cs) remove upgradedb/backupdb options

--- src/Ucommerce.Sitecore.Cli/Commands/Upgrade.cs
+++ src/Ucommerce.Sitecore.Cli/Commands/Upgrade.cs
@@ -7,18 +7,10 @@ namespace Ucommerce.Sitecore.Cli.Commands
     [Command("upgrade")]
     public class Upgrade : Install
     {
-        [CommandOption("backupdb", 'b', Description = "Do a backup of the current database.")]
-        public bool BackupDatabase { get; set; } = false;
-
-        [CommandOption("upgradedb", 'u', Description = "Upgrade the database.")]
-        public bool UpgradeDatabase { get; set; } = false;
-
         public override ValueTask ExecuteAsync(IConsole console)
         {
             console.Output.WriteLine("Upgrading..");
-            if (BackupDatabase) console.Output.WriteLine("Backing up database..");
-            if (UpgradeDatabase) console.Output.WriteLine("Upgrading database..");
-
+         
             return default;
         }
     }
