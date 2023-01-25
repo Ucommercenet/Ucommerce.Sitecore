392bc86c9066af330eabd211d28a5cfb9f6ed29b
Victor-Allan <victor.allan.nielsen@ucommerce.dk>
2023-01-25 14:41:05 +0100

    add toggle active searchprovider steps

--- src/UCommerce.SiteCore.Installer/Steps/InstallStep.cs
+++ src/UCommerce.SiteCore.Installer/Steps/InstallStep.cs
@@ -1,5 +1,6 @@
 using System.IO;
-using Ucommerce.Infrastructure.Configuration;
+using FluentNHibernate.Conventions.AcceptanceCriteria;
+using J2N.Collections.Generic;
 using Ucommerce.Installer;
 
 namespace Ucommerce.Sitecore.Installer.Steps
@@ -22,38 +23,175 @@ namespace Ucommerce.Sitecore.Installer.Steps
                 new InstallDatabaseUcommerce(baseDirectory, connectionStringLocator, loggingService),
                 new InstallDatabaseSitecore(baseDirectory, connectionStringLocator, loggingService),
                 new UpdateUCommerceAssemblyVersionInDatabase(updateService, runtimeVersionChecker, loggingService),
-                new CopyFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, "web.config")), new FileInfo(Path.Combine(sitecoreDirectory.FullName, "web.config.{DateTime.Now.Ticks}.backup")), loggingService),
-                new SitecoreWebconfigMerger(sitecoreDirectory,loggingService),
-                new SeperateConfigSectionInNewFile("configuration/sitecore/settings",new FileInfo(Path.Combine(sitecoreDirectory.FullName,"web.config")),new FileInfo(Path.Combine(sitecoreDirectory.FullName,"App_Config","Include",".Sitecore.Settings.config")),loggingService),
-                new MoveDirectory(new DirectoryInfo(Path.Combine(sitecoreDirectory.FullName,"sitecore modules","shell","ucommerce","install","binaries")),new DirectoryInfo(Path.Combine(sitecoreDirectory.FullName,"bin","uCommerce")),overwriteTarget:true,loggingService),
-                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName,"bin","ucommerce","Ucommerce.Installer.dll")), loggingService),
-                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName,"bin","Ucommerce.Transactions.Payments.dll")), loggingService),
-                new DeleteDirectory(new DirectoryInfo(Path.Combine(appsPath,"ServiceStack")), loggingService),
-                new DeleteDirectory(new DirectoryInfo(Path.Combine(appsPath,"RavenDB25")), loggingService),
-                new DeleteDirectory(new DirectoryInfo(Path.Combine(appsPath,"RavenDB25.disabled")), loggingService),
+                new CopyFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, "web.config")),
+                    new FileInfo(Path.Combine(sitecoreDirectory.FullName, "web.config.{DateTime.Now.Ticks}.backup")),
+                    loggingService),
+                new SitecoreWebconfigMerger(sitecoreDirectory, loggingService),
+                new SeperateConfigSectionInNewFile("configuration/sitecore/settings",
+                    new FileInfo(Path.Combine(sitecoreDirectory.FullName, "web.config")),
+                    new FileInfo(Path.Combine(sitecoreDirectory.FullName, "App_Config", "Include", ".Sitecore.Settings.config")),
+                    loggingService),
+                new MoveDirectory(new DirectoryInfo(Path.Combine(sitecoreDirectory.FullName, "sitecore modules", "shell", "ucommerce", "install", "binaries")),
+                    new DirectoryInfo(Path.Combine(sitecoreDirectory.FullName, "bin", "uCommerce")),
+                    overwriteTarget: true,
+                    loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, "bin", "ucommerce", "Ucommerce.Installer.dll")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, "bin", "Ucommerce.Transactions.Payments.dll")), loggingService),
+                new DeleteDirectory(new DirectoryInfo(Path.Combine(appsPath, "ServiceStack")), loggingService),
+                new DeleteDirectory(new DirectoryInfo(Path.Combine(appsPath, "RavenDB25")), loggingService),
+                new DeleteDirectory(new DirectoryInfo(Path.Combine(appsPath, "RavenDB25.disabled")), loggingService),
                 new DeleteDirectory(new DirectoryInfo(Path.Combine(appsPath, "RavenDB30")), loggingService),
                 new DeleteDirectory(new DirectoryInfo(Path.Combine(appsPath, "RavenDB30.disabled")), loggingService),
-                new MoveDirectory(new DirectoryInfo(Path.Combine(appsPath, "ExchangeRateAPICurrencyConversion.disabled")), new DirectoryInfo(Path.Combine(appsPath, "ExchangeRateAPICurrencyConversion")), true, loggingService),
+                new MoveDirectory(new DirectoryInfo(Path.Combine(appsPath, "ExchangeRateAPICurrencyConversion.disabled")),
+                    new DirectoryInfo(Path.Combine(appsPath, "ExchangeRateAPICurrencyConversion")),
+                    true,
+                    loggingService),
                 new DeleteDirectory(new DirectoryInfo(Path.Combine(appsPath, "Catalogs")), loggingService),
                 new DeleteDirectory(new DirectoryInfo(Path.Combine(appsPath, "Catalogs.disabled")), loggingService),
                 new EnableSitecoreCompatibilityApp(versionChecker, sitecoreDirectory, loggingService),
                 new DeleteDirectory(new DirectoryInfo(Path.Combine(appsPath, "Widgets", "CatalogSearch")), loggingService),
                 new DeleteDirectory(new DirectoryInfo(Path.Combine(appsPath, "Widgets", "CatalogSearch.disabled")), loggingService),
-                new MoveDirectory(new DirectoryInfo(Path.Combine(appsPath, "Sanitization.disabled")),new DirectoryInfo(Path.Combine(appsPath, "Sanitization")), true, loggingService),
+                new MoveDirectory(new DirectoryInfo(Path.Combine(appsPath, "Sanitization.disabled")),
+                    new DirectoryInfo(Path.Combine(appsPath, "Sanitization")),
+                    true,
+                    loggingService),
                 new DeleteFile(new FileInfo(Path.Combine(appsPath, "Sanitization", "bin", "AngleSharp.dll")), loggingService),
                 new DeleteFile(new FileInfo(Path.Combine(appsPath, "Sanitization", "bin", "HtmlSanitizer.dll")), loggingService),
                 new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, "Sanitization", "bin", "HtmlSanitizer.dll")), loggingService),
-                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, "sitecore modules", "shell","ucommerce","Configuration","Payments.config")), loggingService),
-                new MoveUcommerceBinaries(baseDirectory,sitecoreDirectory,loggingService),
-                new MoveResourceFiles(baseDirectory,connectionStringLocator,loggingService),
-                new RenameConfigDefaultFilesToConfigFilesStep(new DirectoryInfo(Path.Combine(sitecoreDirectory.FullName,"sitecore modules","Shell","uCommerce","Configuration")),false,loggingService),
-                new RenameConfigDefaultFilesToConfigFilesStep(new DirectoryInfo(Path.Combine(sitecoreDirectory.FullName,"sitecore modules","Shell","uCommerce","Pipelines")),false,loggingService),
-                new RenameConfigDefaultFilesToConfigFilesStep(new DirectoryInfo(Path.Combine(sitecoreDirectory.FullName,"sitecore modules","Shell","uCommerce","Apps")),false,loggingService),
-                new MoveDirectoryIfTargetExist( new DirectoryInfo(Path.Combine(appsPath, "SimpleInventory.disabled")), new DirectoryInfo(Path.Combine(appsPath, "SimpleInventory")), loggingService),
-                new MoveDirectoryIfTargetExist( new DirectoryInfo(Path.Combine(appsPath, "Acquire and Cancel Payments.disabled")), new DirectoryInfo(Path.Combine(appsPath, "Acquire and Cancel Payments")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName,
+                        "sitecore modules",
+                        "shell",
+                        "ucommerce",
+                        "Configuration",
+                        "Payments.config")),
+                    loggingService),
+                new MoveUcommerceBinaries(baseDirectory, sitecoreDirectory, loggingService),
+                new MoveResourceFiles(baseDirectory, connectionStringLocator, loggingService),
+                new RenameConfigDefaultFilesToConfigFilesStep(
+                    new DirectoryInfo(Path.Combine(sitecoreDirectory.FullName, "sitecore modules", "Shell", "uCommerce", "Configuration")),
+                    false,
+                    loggingService),
+                new RenameConfigDefaultFilesToConfigFilesStep(
+                    new DirectoryInfo(Path.Combine(sitecoreDirectory.FullName, "sitecore modules", "Shell", "uCommerce", "Pipelines")),
+                    false,
+                    loggingService),
+                new RenameConfigDefaultFilesToConfigFilesStep(
+                    new DirectoryInfo(Path.Combine(sitecoreDirectory.FullName, "sitecore modules", "Shell", "uCommerce", "Apps")),
+                    false,
+                    loggingService),
+                new MoveDirectoryIfTargetExist(new DirectoryInfo(Path.Combine(appsPath, "SimpleInventory.disabled")),
+                    new DirectoryInfo(Path.Combine(appsPath, "SimpleInventory")),
+                    loggingService),
+                new MoveDirectoryIfTargetExist(new DirectoryInfo(Path.Combine(appsPath, "Acquire and Cancel Payments.disabled")),
+                    new DirectoryInfo(Path.Combine(appsPath, "Acquire and Cancel Payments")),
+                    loggingService),
             });
+            Steps.AddRange(ToggleActiveSearchProviderSteps(appsPath, loggingService));
         }
-        
+
+        private List<IStep> ToggleActiveSearchProviderSteps(string appsPath, IInstallerLoggingService loggingService)
+        {
+            var luceneAppFolderPath = new DirectoryInfo(Path.Combine(appsPath, "Ucommerce.Search.Lucene"));
+            var luceneAppDisabledFolderPath = new DirectoryInfo(Path.Combine(appsPath, "Ucommerce.Search.Lucene.disabled"));
+            var elasticAppFolderPath = new DirectoryInfo(Path.Combine(appsPath, "Ucommerce.Search.ElasticSearch"));
+            var elasticAppDisabledFolderPath = new DirectoryInfo(Path.Combine(appsPath, "Ucommerce.Search.ElasticSearch.disabled"));
+            var steps = new List<IStep> { };
+            if (Directory.Exists(elasticAppFolderPath.FullName))
+            {
+                steps.Add(new MoveDirectoryIfTargetExist(elasticAppDisabledFolderPath, elasticAppFolderPath, loggingService));
+                steps.Add(new MoveDirectoryIfTargetExist(luceneAppFolderPath, luceneAppDisabledFolderPath, loggingService));
+            }
+
+            return steps;
+        }
+
+        private List<IStep> RemoveRenamedPipelinesSteps(DirectoryInfo sitecoreDirectory, IInstallerLoggingService loggingService)
+        {
+            var pipelinesPath = Path.Combine("sitecore modules", "Shell", "ucommerce", "Pipelines");
+
+            var steps = new List<IStep>
+            {
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "Basket.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "Basket.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "Checkout.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "Checkout.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteCampaignItem.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteCampaignItem.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteCategory.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteCategory.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteDataType.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteDataType.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteDefinition.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteDefinition.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteLanguage.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteLanguage.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteProduct.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteProduct.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteProductCatalog.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteProductCatalog.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteProductCatalogGroup.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteProductCatalogGroup.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteProductDefinitionField.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "DeleteProductDefinitionField.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "Processing.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "Processing.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "ProductReview.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "ProductReview.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "ProductReviewComment.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "ProductReviewComment.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveCategory.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveCategory.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveDataType.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveDataType.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveDefinition.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveDefinition.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveDefinitionField.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveDefinitionField.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveLanguage.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveLanguage.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveOrder.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveOrder.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveProduct.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveProduct.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveProductCatalog.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveProductCatalog.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveProductCatalogGroup.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveProductCatalogGroup.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveProductDefinitionField.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "SaveProductDefinitionField.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "ToCancelled.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "ToCancelled.config")), loggingService),
+
+                new FileBackup(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "ToCompletedOrder.config")), loggingService),
+                new DeleteFile(new FileInfo(Path.Combine(sitecoreDirectory.FullName, pipelinesPath, "ToCompletedOrder.config")), loggingService),
+            };
+            return steps;
+        }
+
         // // Set up search providers
         //  ToggleActiveSearchProvider(virtualAppsPath);
         //
