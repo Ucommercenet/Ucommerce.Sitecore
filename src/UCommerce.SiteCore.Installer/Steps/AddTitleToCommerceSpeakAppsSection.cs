2c82244ddfa815153970d60454c5495490f4a7b4
Skidaddle99 <Mads.Bjernemose.Nielsen@ucommerce.dk>
2023-01-31 10:44:58 +0100

    chore: clean up AddTitleToCommerceSpeakAppsSection.cs
    Did a code cleanup, changed variables to var and added braces to if and while
    
    sc-18385

--- src/UCommerce.SiteCore.Installer/Steps/AddTitleToCommerceSpeakAppsSection.cs
+++ src/UCommerce.SiteCore.Installer/Steps/AddTitleToCommerceSpeakAppsSection.cs
@@ -9,62 +9,69 @@ using Ucommerce.Installer;
 
 namespace Ucommerce.Sitecore.Installer.Steps
 {
-	public class AddTitleToCommerceSpeakAppsSection : IPostStep
-	{
-		public void Run(ITaskOutput output, NameValueCollection metaData)
-		{
-			IInstallerLoggingService logging = new SitecoreInstallerLoggingService();
-			logging.Information<CreateSpeakApplications>("AddTitleToCommerceSpeakAppsSection started.");
+    public class AddTitleToCommerceSpeakAppsSection : IPostStep
+    {
+        public void Run(ITaskOutput output, NameValueCollection metaData)
+        {
+            IInstallerLoggingService logging = new SitecoreInstallerLoggingService();
+            logging.Information<CreateSpeakApplications>("AddTitleToCommerceSpeakAppsSection started.");
 
-			Parse(new DirectoryInfo(GetRootFolder()));
+            Parse(new DirectoryInfo(GetRootFolder()));
 
-			logging.Information<CreateSpeakApplications>("AddTitleToCommerceSpeakAppsSection finished.");
-		}
+            logging.Information<CreateSpeakApplications>("AddTitleToCommerceSpeakAppsSection finished.");
+        }
 
-		private void Parse(DirectoryInfo directoryInfo)
-		{
-			var fileInfos = directoryInfo.GetFiles("*.item");
-			foreach (var fileInfo in fileInfos)
-			{
-				var streamReader = new StreamReader(fileInfo.FullName);
+        private string GetRootFolder()
+        {
+            var serializationFolder = "/Sitecore Modules/Shell/ucommerce/install/SpeakSerialization/sitecore82";
+            var str = Path.GetFullPath(SafeMapPath(serializationFolder));
+            if (str[str.Length - 1] != Path.DirectorySeparatorChar)
+            {
+                str = str + Path.DirectorySeparatorChar;
+            }
 
-				var syncItem = SyncItem.ReadItem(new Tokenizer(streamReader), true);
-				var options = new LoadOptions { DisableEvents = true, ForceUpdate = true, UseNewID = false };
+            return str;
+        }
 
-				ItemSynchronization.PasteSyncItem(syncItem, options, true);
-			}
+        private void Parse(DirectoryInfo directoryInfo)
+        {
+            var fileInfos = directoryInfo.GetFiles("*.item");
+            foreach (var fileInfo in fileInfos)
+            {
+                var streamReader = new StreamReader(fileInfo.FullName);
 
-			foreach (var info in directoryInfo.GetDirectories())
-			{
-				Parse(info);
-			}
-		}
-		private string GetRootFolder()
-		{
-			string serializationFolder = "/Sitecore Modules/Shell/ucommerce/install/SpeakSerialization/sitecore82";
-			string str = Path.GetFullPath(SafeMapPath(serializationFolder));
-			if (str[str.Length - 1] != Path.DirectorySeparatorChar)
-				str = str + Path.DirectorySeparatorChar;
-			return str;
-		}
+                var syncItem = SyncItem.ReadItem(new Tokenizer(streamReader), true);
+                var options = new LoadOptions { DisableEvents = true, ForceUpdate = true, UseNewID = false };
 
-		private static string SafeMapPath(string path)
-		{
-			while (!string.IsNullOrEmpty(path) && path[0] == '/')
-				path = path.Substring(1);
-			return Path.Combine(GetSafeAppRoot(), path ?? string.Empty);
-		}
+                ItemSynchronization.PasteSyncItem(syncItem, options, true);
+            }
 
-		private static string GetSafeAppRoot()
-		{
-			try
-			{
-				return FileUtil.MapPath("/");
-			}
-			catch (Exception)
-			{
-				return AppDomain.CurrentDomain.BaseDirectory;
-			}
-		}
-	}
+            foreach (var info in directoryInfo.GetDirectories())
+            {
+                Parse(info);
+            }
+        }
+
+        private static string GetSafeAppRoot()
+        {
+            try
+            {
+                return FileUtil.MapPath("/");
+            }
+            catch (Exception)
+            {
+                return AppDomain.CurrentDomain.BaseDirectory;
+            }
+        }
+
+        private static string SafeMapPath(string path)
+        {
+            while (!string.IsNullOrEmpty(path) && path[0] == '/')
+            {
+                path = path.Substring(1);
+            }
+
+            return Path.Combine(GetSafeAppRoot(), path ?? string.Empty);
+        }
+    }
 }
