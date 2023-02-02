509f52ad9eb557d5a89134833c1cfb9dff5f9905
Skidaddle99 <Mads.Bjernemose.Nielsen@ucommerce.dk>
2023-02-02 14:14:48 +0100

    chore: Add xml-documentation for the DirectoryInfo extension methods
    To add clarity in the future, xml documentation should be added to the DirectoryInfo extension methods
    
    sc-18628

--- src/UCommerce.SiteCore.Installer/FileExtensions/DirectoryExtensions.cs
+++ src/UCommerce.SiteCore.Installer/FileExtensions/DirectoryExtensions.cs
@@ -2,14 +2,29 @@
 
 namespace Ucommerce.Sitecore.Installer.FileExtensions
 {
+    /// <summary>
+    /// Extension methods used for the DirectoryInfo object
+    /// </summary>
     internal static class DirectoryExtensions
     {
+        /// <summary>
+        /// Combines the path of the directory and the given strings
+        /// </summary>
+        /// <param name="directory">The directory that defines the first part of the path</param>
+        /// <param name="paths">The strings that make up the rest of the path</param>
+        /// <returns>A new DirectoryInfo based on the combined path</returns>
         internal static DirectoryInfo CombineDirectory(this DirectoryInfo directory, params string[] paths)
         {
             var path = Path.Combine(directory.FullName, Path.Combine(paths));
             return new DirectoryInfo(path);
         }
 
+        /// <summary>
+        /// Combines the path of the directory and the given strings
+        /// </summary>
+        /// <param name="directory">The directory that defines the first part of the path</param>
+        /// <param name="paths">The strings that make up the rest of the path</param>
+        /// <returns>A new FileInfo based on the combined path</returns>
         internal static FileInfo CombineFile(this DirectoryInfo directory, params string[] paths)
         {
             var path = Path.Combine(directory.FullName, Path.Combine(paths));
