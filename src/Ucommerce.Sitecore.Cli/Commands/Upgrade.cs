e812255650862617544bac740ed17d55b36551db
Skidaddle99 <Mads.Bjernemose.Nielsen@ucommerce.dk>
2023-02-09 15:38:55 +0100

    chore: added descriptions to the install and upgrade commands
    sc-18691

--- src/Ucommerce.Sitecore.Cli/Commands/Upgrade.cs
+++ src/Ucommerce.Sitecore.Cli/Commands/Upgrade.cs
@@ -2,6 +2,6 @@
 
 namespace Ucommerce.Sitecore.Cli.Commands
 {
-    [Command("upgrade")]
+    [Command("upgrade", Description = "Upgrade the current Ucommerce installation.")]
     public class Upgrade : Install { }
 }
