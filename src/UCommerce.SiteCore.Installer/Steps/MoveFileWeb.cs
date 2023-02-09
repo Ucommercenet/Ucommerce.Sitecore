c477a408ef1410df201e314d59005a175ac33fe7
Skidaddle99 <Mads.Bjernemose.Nielsen@ucommerce.dk>
2023-02-09 10:03:06 +0100

    chore: add ConfigIncludes to old steps
    added back some old code that replaces sitecore config files with empty ones, used to be able to boot up sitecore when the dlls are not available
    
    sc-18673

---
+++ src/UCommerce.SiteCore.Installer/Steps/MoveFileWeb.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Specialized;
+using System.IO;
+using System.Web.Hosting;
+using Sitecore.Install.Framework;
+using Ucommerce.Installer;
+
+namespace Ucommerce.Sitecore.Installer.Steps
+{
+    public class MoveFileWeb : IPostStep
+    {
+        private readonly bool _backupTarget;
+        private readonly FileMover _command;
+
+        public MoveFileWeb(string sourceVirtualPath, string targetVirtualPath, bool backupTarget)
+        {
+            _backupTarget = backupTarget;
+
+            FileInfo source = new FileInfo(HostingEnvironment.MapPath(sourceVirtualPath)),
+                target = new FileInfo(HostingEnvironment.MapPath(targetVirtualPath));
+
+            _command = new FileMover(source, target);
+        }
+
+        public void Run(ITaskOutput output, NameValueCollection metaData)
+        {
+            _command.Move(_backupTarget, ex => new SitecoreInstallerLoggingService().Error<int>(ex));
+        }
+    }
+}
