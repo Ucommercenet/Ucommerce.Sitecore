4dec002ceabd72c167e566c1465b84fddb2dfc68
Skidaddle99 <Mads.Bjernemose.Nielsen@ucommerce.dk>
2023-02-10 11:17:33 +0100

    chore: add "Install sitecore package" message
    Changed the starting message slightly, and added a small list of instructions for installing the package at the end of the CLI process.
    
    sc-18691

--- src/Ucommerce.Sitecore.Cli/Commands/Install.cs
+++ src/Ucommerce.Sitecore.Cli/Commands/Install.cs
@@ -37,7 +37,10 @@ namespace Ucommerce.Sitecore.Cli.Commands
         public virtual async ValueTask ExecuteAsync(IConsole console)
         {
             var logging = new CliLogger(console);
-            logging.Information<Install>("Installing...");
+            console.BackgroundColor = ConsoleColor.Black;
+            console.ForegroundColor = ConsoleColor.White;
+            await console.Output.WriteLineAsync("Installing Ucommerce for Sitecore...");
+            console.ResetColor();
 
             var baseDirectory = new DirectoryInfo(AppContext.BaseDirectory);
             var sitecoreDirectory = new DirectoryInfo(SitecorePath);
@@ -61,7 +64,17 @@ namespace Ucommerce.Sitecore.Cli.Commands
                 sitecoreDirectory,
                 versionChecker,
                 logging);
-            await installStep.Run();
+            //await installStep.Run();
+
+            console.BackgroundColor = ConsoleColor.Black;
+            console.ForegroundColor = ConsoleColor.White;
+            await console.Output.WriteLineAsync("File installation completed! Next, you will have to install a package in Sitecore."
+                + "\n1. Login to Sitecore"
+                + "\n2. Go to Sitecore desktop"
+                + "\n3. In the bottom left menu, go to 'Development Tools' -> 'Installation Wizard'"
+                + $"\n4. Click 'Choose package', click on 'Ucommerce-PostInstall-{runtimeVersionChecker.GetUcommerceRuntimeAssemblyVersion()}.zip' and go through the installation process"
+                + "\nAfter that, the Ucommerce tab should be visible, and good to go!");
+            console.ResetColor();
         }
     }
 }
